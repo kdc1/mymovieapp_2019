7d604682822a667bb10078ae263e8e6a96f193da
owner <kdc_1@naver.com>
2019-11-18 14:59:09 +0900

    proptype 까지 학습

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from "prop-types";
 
+function Food ({name, picture, rating }){
+  return (
+  <div>
+    <h2>I know {name}</h2>
+    <h4>{rating}/5.0</h4>
+    <img src={picture} alt={name} />
+  </div>
+  );
+}
+
+const foodILike = [
+  {
+    id: 1,
+    name: "Kimchi",
+    image:
+    "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+    rating: 5
+
+},
+{
+  id:2,
+  name:"Samgyeopsal",
+  image:
+  "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+  rating: 4.9
+
+}
+];
+function renderFood(dish){
+  console.log(dish);
+  return <Food name={dish.name} picture={dish.image} />
+
+}
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number
+
+};
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {console.log(foodILike.map(renderFood))}
+        {foodILike.map(dish =>(
+          <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
+        ))}
       </div>
+      
     );
   }
 }
